2abdfc8d90c2a9d222bab62a75805367a47a16e3
JustYoomoon <yangxiao@tutamail.com>
2021-06-06 15:51:24 +0800

    update

--- vulsearch.py
+++ vulsearch.py
@@ -1,36 +1,47 @@
 from mediawiki import MediaWiki
 import sys
 import json
-
+import os
+import argparse
+import requests
+#from prettytable import PrettyTable
 pwnwiki = MediaWiki(user_agent='Mozilla/5.0 (Windows NT 10.0; WOW64')
 pwnwiki = MediaWiki(url='https://www.pwnwiki.org/api.php')
 
-#get info
-try:
-    keyword = sys.argv[1]
-except:
-    print('+---------------------------------------------------------------------------------------------------------+')
-    print('+ DES: by PwnWiki as https://www.pwnwiki.prg                                                              +')
-    print('+      Free, editable vulnerability library for everyone                                                  +')
-    print('+---------------------------------------------------------------------------------------------------------+')
-    print('+ USE: vulsearch <NAME>                                                                                   +')
-    print('+ vulsearch -update / vulsearch -language en <NAME>  /  vulsearch -local <NAME> / vulsearch -v            +')
-    print('+ VER: PwnDatas-DB-Project(PDDP) & vulsearch May.27 2021                                                  +')
-    print('+---------------------------------------------------------------------------------------------------------+')
-    sys.exit(0)
 
-web_data = pwnwiki.opensearch(keyword, results=20)  #最多輸出20個結果
+parser = argparse.ArgumentParser(usage='%(prog)s [options]')
+parser.add_argument("-v", "--version", help="Output program version", action="store_true")
+parser.add_argument("-s", "--search", help="Search from PwnWiki",action='append',nargs = '*')
+parser.add_argument("-l", "--language", help="Specify language search",action='append',nargs = '*')
 
-#print(web_data)
-s = json.dumps(web_data)
-#print(s)
-s1 = json.loads(s)
-#print(s1)
+args = parser.parse_args()
 
-try:
-    for i in range(0,20) :
-        print("\033[32m[+]Vulnerability\033[0m" ,s1[i][0])  #標題
-        print("\033[33m[+]URL\033[0m" ,s1[i][2])  #網址
-        print("---------------------------------------------------------------------------------------------------------------")
-except:
-    pass
+if args.version:
+    print("\033[1;34m[Version]\033[0m PwnDatas-DB-Project(PDDP) & vulsearch Jun.6 2021") #Blue
+
+if args.search:
+    keyword = args.search[0]
+    keyword = ','.join(str(i) for i in keyword)
+    web_data = pwnwiki.opensearch(keyword, results=20)
+    s = json.dumps(web_data)
+    s1 = json.loads(s)
+    #print(s1) #Success
+    try:
+        for i in range(0,20):
+            display = ("https://www.pwnwiki.org/api.php?action=parse&page=%s&contentmodel=wikitext&format=json" %(s1[i][0]))
+                #print("\033[32m[+]Vulnerability\033[0m" ,s1[i][0])  #標題
+            response = requests.get(display)
+            #print(response)
+            content = response.text
+            json_dict = json.loads(content)
+                #print(json_dict)
+            print("\033[32m[+]Vulnerability\033[0m" ,json_dict['parse']['displaytitle'])
+                #print(json_dict['parse']['displaytitle'])
+            print("\033[33m[+]URL\033[0m" ,s1[i][2])  #網址
+
+            #print(display)
+            #print(display_json)
+            print('------------------------------------------------------------------------------------------------------')
+    except:
+        pass
+
