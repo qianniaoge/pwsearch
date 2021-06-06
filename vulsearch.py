a9ab4671d76ade072a92f4d944bf76d16d001430
JustYoomoon <yangxiao@tutamail.com>
2021-06-06 16:47:19 +0800

    Update vulsearch.py
    update

--- vulsearch.py
+++ vulsearch.py
@@ -13,16 +13,24 @@ parser = argparse.ArgumentParser(usage='%(prog)s [options]')
 parser.add_argument("-v", "--version", help="Output program version", action="store_true")
 parser.add_argument("-s", "--search", help="Search from PwnWiki",action='append',nargs = '*')
 parser.add_argument("-l", "--language", help="Specify language search",action='append',nargs = '*')
+parser.add_argument("-n", "--number", help="Number of pages",action='append',nargs = '*')
 
 args = parser.parse_args()
 
 if args.version:
     print("\033[1;34m[Version]\033[0m PwnDatas-DB-Project(PDDP) & vulsearch Jun.6 2021") #Blue
 
+if args.number:
+    number = args.number[0]
+    number = ','.join(str(i) for i in number)
+    #print(number)
+
 if args.search:
     keyword = args.search[0]
     keyword = ','.join(str(i) for i in keyword)
-    web_data = pwnwiki.opensearch(keyword, results=20)
+    print("\033[1;34m[INFO]\033[0m According to your settings, the program outputs %s pieces of content." %(number))
+    number = int(number)
+    web_data = pwnwiki.opensearch(keyword, results=number)
     s = json.dumps(web_data)
     s1 = json.loads(s)
     #print(s1) #Success
@@ -44,4 +52,3 @@ if args.search:
             print('------------------------------------------------------------------------------------------------------')
     except:
         pass
-
